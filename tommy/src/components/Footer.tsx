f8bfd49585809ae9dfd3d0eb11f39b3678508249
drew-fugate <drew.fugate@wavelengthusaf.com>
2023-05-12 14:45:08 -0500

    made social buttons work

--- tommy/src/components/Footer.tsx
+++ tommy/src/components/Footer.tsx
@@ -6,6 +6,10 @@ import Instagram from "../images/Instagram.png";
 import YouTube from "../images/YouTube.png";
 
 export default function Footer() {
+  const goToURL = (props: string) => {
+    window.open(props, "_blank");
+  };
+
   return (
     <div className="footer">
       <div className="hutlet">
@@ -17,11 +21,31 @@ export default function Footer() {
         <p>ABOUT</p>
       </div>
       <div className="socialButtons">
-        <img src={TikTok} alt="TikTok"></img>
-        <img src={Twitch} alt="Twitch"></img>
-        <img src={Twitter} alt="Twitter"></img>
-        <img src={Instagram} alt="Instagram"></img>
-        <img src={YouTube} alt="YouTube"></img>
+        <img
+          src={TikTok}
+          alt="TikTok"
+          onClick={() => goToURL("https://www.spotify.com")}
+        ></img>
+        <img
+          src={Twitch}
+          alt="Twitch"
+          onClick={() => goToURL("https://www.twitch.tv")}
+        ></img>
+        <img
+          src={Twitter}
+          alt="Twitter"
+          onClick={() => goToURL("https://www.twitter.com")}
+        ></img>
+        <img
+          src={Instagram}
+          alt="Instagram"
+          onClick={() => goToURL("https://www.instagram.com")}
+        ></img>
+        <img
+          src={YouTube}
+          alt="YouTube"
+          onClick={() => goToURL("https://www.youtube.com")}
+        ></img>
       </div>
     </div>
   );
